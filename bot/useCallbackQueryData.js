d730c70308fef1b5053f18eefcf290a8c6b1d81c
Vyacheslav Aristov <vv.aristov@gmail.com>
2023-11-28 11:46:26 +0300

    Formatting

--- bot/useCallbackQueryData.js
+++ bot/useCallbackQueryData.js
@@ -1,5 +1,9 @@
-import { DateTime, Interval } from 'luxon'
+import { DateTime, Duration } from 'luxon'
 import calendar from './calendar.js'
+
+const hour = Duration.fromObject({ hours : 1 })
+const hourAsMinutes = hour.as('minutes')
+const hourAsMilliseconds = hour.as('milliseconds')
 
 export async function useCallbackQueryData(ctx) {
   const start = DateTime.fromISO(ctx.callbackQuery.data)
@@ -20,9 +24,22 @@ export async function useCallbackQueryData(ctx) {
       },
     },
   })
-  const interval = Interval.fromDateTimes(start, end)
-  const time = interval.toLocaleString(DateTime.TIME_24_SIMPLE)
+  const startString = start.toFormat('H:mm')
+  const endString = end.toFormat('H:mm')
+  const units = end - start < hourAsMilliseconds ?
+    ['minutes'] :
+    ['hours', 'minutes']
+  const diff = end.diff(start, units)
+  const minutes = Math.round(diff.as('minutes'))
+  const hours = Math.floor(minutes / hourAsMinutes)
+  const duration = Duration.fromObject({
+    hours : hours || undefined,
+    minutes : minutes % hourAsMinutes,
+  })
+  const durationString = duration.toHuman()
   await ctx.deleteMessage()
-  await ctx.reply(`Sleep log entry added: ${ time }`)
+  await ctx.reply(
+    `Sleep log entry added\n\n${ startString } — ${ endString }\n${ durationString }`,
+  )
   await ctx.answerCallbackQuery()
 }
