c5f631e59574245f7828defab7d55a1ef6769abc
Vyacheslav Aristov <vv.aristov@gmail.com>
2025-02-19 08:40:48 +0300

    Support for the REPLY_DELAY environment variable

--- bot/useLightsOut.js
+++ bot/useLightsOut.js
@@ -3,12 +3,14 @@ import { InlineKeyboard } from 'grammy'
 
 const wakeUpButtonText = process.env.WAKEUP_BUTTON_TEXT || `I'm awake`
 const lightsOutText = process.env.LIGHTSOUT_TEXT || 'OK, click the button below when you wake up'
+const delay = process.env.REPLY_DELAY || 0
 
 export async function useLightsOut(ctx) {
   const now = DateTime.now()
   const keyboard = InlineKeyboard.from([
     [InlineKeyboard.text(wakeUpButtonText, now.toISO())],
   ])
+  await new Promise(resolve => setTimeout(resolve, +delay))
   await ctx.reply(lightsOutText, {
     reply_markup : keyboard,
   })
